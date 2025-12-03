0abfaa4b6cbdbff5da337c6150a29586c9d6bdc9
Chigoziri <ChigoziriOnwenu@gmail.com>
2025-12-03 15:35:24 -0600

    fixed prisma import, removed files api endpoint (unused)

--- admin-portal/src/app/api/documents/[id]/route.ts
+++ admin-portal/src/app/api/documents/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { prisma } from "../../../../../lib/prisma";
+import { prisma } from "../../../../lib/prisma";
 import path from "path";
 import { readFile, unlink } from "fs/promises";
 
